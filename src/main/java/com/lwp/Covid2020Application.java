cabefcad1deed5b61913eee1839364c13f4a26a2
Brian <unasbrian@gmail.com>
2020-04-18 20:02:39 -0500

    first version

---
+++ src/main/java/com/lwp/Covid2020Application.java
@@ -0,0 +1,13 @@
+package com.lwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid2020Application.class, args);
+	}
+
+}
